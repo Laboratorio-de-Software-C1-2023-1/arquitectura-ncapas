ae8ea8d19643841c749bf09ccf8da8700c9f4d59
Daniel Fernandez <dfernandez@unpaz.edu.ar>
2023-05-09 19:33:59 -0300

    Initial commit

---
+++ 02-Dominio/Usuario.cs
@@ -0,0 +1,31 @@
+﻿namespace _02_Dominio
+{
+    public class Usuario
+    {
+        private String nombre;
+        private String email;
+        private int edad;
+
+        public Usuario(
+            String nombre,
+            String email,
+            int edad
+        )
+        {
+
+            if (this.nombre.Length < 4)
+            {
+                throw new Exception("El nombre debe contener, al menos, 4 caractreres");
+            }
+
+            if (this.edad < 18)
+            {
+                throw new Exception("El usuario debe ser mayor a 18 años");
+            }
+
+            this.nombre = nombre;
+            this.email = email;
+            this.edad = edad;
+        }
+    }
+}
